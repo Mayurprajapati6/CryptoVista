526c03dc15596ad7f183be34264ac16e02d4781d
Mayur Prajapati <mproyur685@gmail.com>
2025-06-14 23:26:16 +0530

    Added Routing and created Home and about project

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,11 @@ import './App.css'
 import Navbar from './components/Navbar/Navbar';
 import Banner from './components/Banner/Banner';
 import CoinTable from './components/CoinTable/CoinTable';
+import Routing from './components/Routing/Routing';
 
 function App() {
   return (
-    <div>
-      
-      <Banner />
-      <CoinTable />
-    </div>
+    <Routing />
   );
 }
 
