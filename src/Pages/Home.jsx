c90b96c12c4927037d83ffbff2ffe6e18133667d
Mayur Prajapati <mproyur685@gmail.com>
2025-06-20 22:06:30 +0530

    resolved one bug

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from '../Components/Navbar/Navbar'
+
 import Banner from '../Components/Banner/Banner'
 import CryptoCoinTable from '../components/CryptoCoinTable/CryptoCoinTable'
 function Home() {
